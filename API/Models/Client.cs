a39536272423904adb24fcaf7aa11b061e114ddd
alex <alex.pap94@hotmail.gr>
2022-12-01 20:38:01 +0200

    NUTR-6 - code review changes

--- API/Models/Client.cs
+++ API/Models/Client.cs
@@ -4,9 +4,10 @@ public class Client
 {
     public int Id { get; set; }
     public bool PasswordChanged { get; set; }
-    public DateTime MyProperty { get; set; }
+    public DateTime DateOfBirth { get; set; }
     public string PhoneNumber { get; set; }
     public float Height { get; set; }
     public Nutritionist? Nutritionist { get; set; }
+    public List<Measurement> Measurements { get; set; }
     public AppUser AppUser { get; set; }
 }
