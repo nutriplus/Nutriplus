7dcb84968200a6e5dc25633a2350b508205d60cb
alex <alex.pap94@hotmail.gr>
2022-12-01 19:32:29 +0200

    NUTR-6 - code review changes

--- API/Models/AppUser.cs
+++ API/Models/AppUser.cs
@@ -9,4 +9,5 @@ public class AppUser
     public string Email { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public Nutritionist Nutritionist { get; set; }
 }
--- API/Models/Nutritionist.cs
+++ API/Models/Nutritionist.cs
@@ -1,9 +1,7 @@
 namespace API.Models;
 
-      public class Nutritionist
-       {
-           public int Id { get; set; }
-           public AppUser AppUser { get; set; }
-           public DateTime CreatedAt { get; set; }
-           public DateTime UpdatedAt { get; set; }
+        public class Nutritionist
+        {
+            public int Id { get; set; }
+            public AppUser AppUser { get; set; }
         }
